8e68d6102f3743c8a2b2444bacf8561084712510
Väinö Heijari <vai.hei@hotmail.com>
2024-02-19 19:29:39 +0200

    Imgui integrated

--- Game.cpp
+++ Game.cpp
@@ -4,6 +4,10 @@
 
 #include "Game.h"
 #include <stdexcept>
+#include <imgui.h>
+#include "imgui_stdlib.h"
+#include "imgui_impl_opengl3.h"
+#include "imgui_impl_glfw.h"
 
 Game::Game(const Config& cfg)
 {
@@ -11,19 +15,24 @@ Game::Game(const Config& cfg)
     // ------------------------------
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     mWindow = glfwCreateWindow(cfg.windowWidth, cfg.windowHeight, cfg.windowName.c_str(), nullptr, nullptr);
     if (!mWindow) {
         throw std::runtime_error("Failed to create window");
     }
-
     glfwMakeContextCurrent(mWindow);
 
     if (!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress)) {
         throw std::runtime_error("Failed to initialize glad");
     }
+
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGui::StyleColorsDark();
+    ImGui_ImplGlfw_InitForOpenGL(mWindow, true);
+    ImGui_ImplOpenGL3_Init("#version 150");
 
     glEnable(GL_DEPTH_TEST);
 }
@@ -38,14 +47,23 @@ void Game::run()
     while (!glfwWindowShouldClose(mWindow))
     {
         glfwPollEvents();
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
         handleEvent();
 
         update(mTimer.tick());
 
+        ImGui::ShowDemoWindow();
+
+        ImGui::Render();
         glClearColor(1.00f, 0.05f, 0.05f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         render();
+
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
         glfwSwapBuffers(mWindow);
     }
